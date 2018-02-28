3b3c09bbc8968ea0b012ea7eb61e89d07f058461
Andrew Krigline <andrew.krigline@robotsandpencils.com>
2018-02-28 13:16:38 -0500

    Changed SVG to an unoptimized version and programatically set initial stroke settings

--- src/components/chinaAnimated/chinaAnimated.js
+++ src/components/chinaAnimated/chinaAnimated.js
@@ -3,9 +3,16 @@ import styles from './chinaAnimated.scss'
 import ChinaMap from '../../assets/svgs/chinaHigh.svg'
 
 class ChinaAnimated extends Component {
+  componentDidMount () {
+    const paths = this.chinaContainer.getElementsByTagName('path')
+    for (let path of paths) {
+      path.setAttribute('stroke-dasharray', path.getTotalLength())
+      path.setAttribute('stroke-dashoffset', path.getTotalLength())
+    }
+  }
   render () {
     return (
-      <div>
+      <div ref={(div) => { this.chinaContainer = div }}>
         <ChinaMap className={styles.chinaMap} />
       </div>
     )
