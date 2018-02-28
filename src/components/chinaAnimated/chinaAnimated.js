f5beae1a2f3b1a40496b211b93f28c42318f60c9
Andrew Krigline <andrew.krigline@robotsandpencils.com>
2018-02-28 16:43:22 -0500

    Added knobs, made more timout-y things happen with the china stroke drawing

--- src/components/chinaAnimated/chinaAnimated.js
+++ src/components/chinaAnimated/chinaAnimated.js
@@ -6,10 +6,29 @@ import ChinaMapOptimized from '../../assets/svgs/chinaHigh-optimized.svg'
 class ChinaAnimated extends Component {
   componentDidMount () {
     const paths = this.chinaContainer.getElementsByTagName('path')
+    let animationDelay = this.props.animationDelay
+    // getElementsByTagName doesn't return an array, it returns an HTMLCollection, so we can't use .map or .forEach
     for (let path of paths) {
+      // Set our Stroke Dash array and dash offset per the length of the line for each line (this is how it draws itself)
       path.setAttribute('stroke-dasharray', path.getTotalLength())
       path.setAttribute('stroke-dashoffset', path.getTotalLength())
+      // Set the Animation Delay as expected before we increment it
+      path.style.animationDelay = `${animationDelay}ms`
+      // Set the Animation duration and transition duration from props
+      path.style.animationDuration = `${this.props.animationDuration}ms`
+      path.style.transitionDuration = `${this.props.transitionDuration}ms`
+      // Incrememnt the animation delay
+      animationDelay = animationDelay + this.props.animationDelay
+      // Set a timeout to coincide with when the line will be finished drawing to do other stuff to that path.
+      setTimeout(() => {
+        path.style.fillOpacity = 1
+        path.style.strokeWidth = 1
+      }, animationDelay + this.props.animationDuration)
     }
+    // Set a timeout to coincide with when all the lines will be finished drawing to do some other stuff!
+    setTimeout(() => {
+      console.log('finished')
+    }, paths.length * this.props.animationDelay + this.props.animationDuration)
   }
   render () {
     return (
--- src/components/chinaAnimated/chinaAnimated.story.js
+++ src/components/chinaAnimated/chinaAnimated.story.js
@@ -9,3 +9,15 @@ storiesOf('ChinaAnimated', module)
   .add('SVG Optimized with vscode', () => (
     <ChinaAnimated />
   ))
+  .add('unoptimized with animation delay of 75', () => (
+    <ChinaAnimated
+      animationDelay={75}
+      animationDuration={300}
+      transitionDuration={300} />
+  ))
+  .add('unoptimized with animation delay of 300', () => (
+    <ChinaAnimated
+      animationDelay={300}
+      animationDuration={300}
+      transitionDuration={300} />
+  ))
