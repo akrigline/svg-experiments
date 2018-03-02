2e458ba73d38cd4c7c2ab2bd4ec26ee6e1cf66ca
Andrew Krigline <andrew.krigline@robotsandpencils.com>
2018-03-02 15:59:15 -0500

    Removed map beginnings

--- src/components/mapHover/mapHover.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import MapHover from './mapHover'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<MapHover />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
